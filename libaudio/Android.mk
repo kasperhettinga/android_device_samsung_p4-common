1a23de8ed0099910a79d2c7bf5078705112f25a2
kasperhettinga <kasper.hettinga@gmail.com>
2013-12-15 09:58:14 +0100

    Add missing libraries

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -8,6 +8,8 @@ LOCAL_MODULE := audio.primary.p3
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_STATIC_LIBRARIES:= libmedia_helper
 LOCAL_SHARED_LIBRARIES:= \
+        libdl \
+        liblog \
 	libutils \
 	libhardware_legacy \
 	libtinyalsa \
